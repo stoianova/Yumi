fef5976d9320e8d505adc380b4ef5a2a1e2b523a
stoianova <stoianova.as@gmail.com>
2024-05-05 02:48:00 +0200

    media

--- src/components/compHeader/Header.jsx
+++ src/components/compHeader/Header.jsx
@@ -4,7 +4,7 @@ import '../compHeader/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBasketShopping, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Body from '../compBody/Section'
-import Login from '../FormsBackEnd/Login'
+// import Login from '../FormsBackEnd/Login'
 
 
 const Header = ({size, setShow}) => {
@@ -38,7 +38,7 @@ const Header = ({size, setShow}) => {
                         
                     </div>
                         
-                        <button className='loginBtn'> <a href="https://coffeemarket-775b0b283547.herokuapp.com/admin/" /> </button>
+                        <button className='loginBtn'> <a href='https://coffeemarket-775b0b283547.herokuapp.com/login/' /> </button>
 
 
 
